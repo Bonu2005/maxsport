832fe3fe30edf851de42f0976f18fffdfb8cf7af
Bonu2005 <booonu@icloud.com>
2026-01-11 12:28:47 +0500

    host

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     app.enableCors({
     origin: [
-   "https://master-fitness.netlify.app"
+   "https://master-fitness.netlify.app",
+   "http://localhost:3000"
     ], // сюда добавляйте нужные фронтенд-домены
     credentials: false,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
